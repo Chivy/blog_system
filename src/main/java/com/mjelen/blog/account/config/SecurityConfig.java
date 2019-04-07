b8540dd71beb0857f99d5ffbfe0ec9c3eeff5f91
Chivy <maciej.jelen95@gmail.com>
2019-04-07 15:09:25 +0200

    added BCrypt password encoding

--- src/main/java/com/mjelen/blog/account/config/SecurityConfig.java
+++ src/main/java/com/mjelen/blog/account/config/SecurityConfig.java
@@ -2,32 +2,35 @@ package com.mjelen.blog.account.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication();
+        auth.jdbcAuthentication().passwordEncoder(bcrypt());
+    }
+
+
+    private BCryptPasswordEncoder bcrypt() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
+        http
+                .authorizeRequests()
+                .antMatchers("/home", "/posts").permitAll()
+                .antMatchers("/login", "/register").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .httpBasic()
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/home")
-                .invalidateHttpSession(true);
-
-
+                .httpBasic();
     }
 }
---
+++ src/main/java/com/mjelen/blog/account/config/SecurityWebAppInitlalizer.java
@@ -0,0 +1,6 @@
+package com.mjelen.blog.account.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitlalizer extends
+        AbstractSecurityWebApplicationInitializer {}
